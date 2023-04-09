2586c559bbe22c9a79fb2d8c953f7a1e46c70f7b
Corey Hawthorne <chawthorne02@gmail.com>
2023-04-09 14:42:42 -0400

    added text to give brief explanation about me

--- src/components/Aboutpage/Aboutpage.js
+++ src/components/Aboutpage/Aboutpage.js
@@ -9,8 +9,13 @@ function AboutPage() {
 
 
     return (
-        <div>
+        <div className="about-page">
             <img src={Me} alt="me" className="me-pic"></img>
+            <p className="about-text">
+                Former educator transitioning into Front End Developer role after completing Carolina Code School. 
+                I have experience with HTML, CSS, JavaScript, BootStrap, React, Python, Django, React BootStrap ,Rest API'S and 
+                I understand the importance of creating highly readable and easily maintainable source code with experience in Agile methodology.
+            </p>
         </div>
     )
 }
