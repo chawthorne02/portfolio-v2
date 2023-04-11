32a24d3d3060ea306499940896e07f284ebe9cb7
Corey Hawthorne <chawthorne02@gmail.com>
2023-04-11 19:23:44 -0400

    added styles page for the project section

--- src/components/Contactpage/Contactpage.js
+++ src/components/Contactpage/Contactpage.js
@@ -1,5 +1,7 @@
 import "../../styles/Contactpage.css"
-
+import { BsFillTelephoneFill } from "react-icons/bs";
+import { BsEnvelopeFill } from "react-icons/bs";
+import { MdLocationOn } from "react-icons/md";
 
 function Contactpage() {
 
@@ -10,8 +12,17 @@ function Contactpage() {
 
     return (
         <div className="contact-page">
-            <h2>Get In Touch</h2>
-
+            <h2 className="contact-title">Get In Touch</h2>
+            <p className="contact">
+                <BsFillTelephoneFill />  864-293-0404
+            </p>
+            <p className="contact">
+                <a href="mailto: chawthorne02@gmail.com"><BsEnvelopeFill /> chawthorne02@gmail.com </a>
+            </p>
+            <p className="contact">
+                <MdLocationOn /> Greenville, South Carolina
+            </p>
+            
         </div>
     )
 }
