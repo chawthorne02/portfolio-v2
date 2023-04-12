eb61cf670f836dbedf2a554a348287472c521275
Corey Hawthorne <chawthorne02@gmail.com>
2023-04-12 15:36:30 -0400

    added border around contact form

--- src/components/Contactpage/Contactform.js
+++ src/components/Contactpage/Contactform.js
@@ -9,14 +9,20 @@ function Contactform() {
 
 
     return (
-        <Form>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+     <Form className='form-section'>
+        <section className='form-left'>
+        <Form.Group className="contact-name">
+        <Form.Label>Name</Form.Label>
+        <Form.Control type="name" placeholder="Your Name..." />
+      </Form.Group>
+      <Form.Group className="contact-email">
         <Form.Label>Email address</Form.Label>
         <Form.Control type="email" placeholder="Email address..." />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Label>Example textarea</Form.Label>
-        <Form.Control as="textarea" rows={3} placeholder='Your message...' />
+      </section>
+      <Form.Group className="contact-message">
+        <Form.Label>Message</Form.Label>
+        <Form.Control as="textarea" rows={10} placeholder='Your message...' />
       </Form.Group>
     </Form>
     )
