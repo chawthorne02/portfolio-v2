323710d230b7de3ed22fb2a782ce7fa228d8bb71
Corey Hawthorne <chawthorne02@gmail.com>
2023-04-14 12:39:34 -0400

    added social links for contact section

---
+++ src/components/Contactpage/Contactsocials.js
@@ -0,0 +1,20 @@
+import { BsLinkedin } from "react-icons/bs";
+import { BsGithub } from "react-icons/bs";
+
+function Contactsocials() {
+
+
+
+
+
+    return (
+        <ul className="contact-socials">
+            <li>
+                <a href="https://www.linkedin.com/in/corey-hawthorne-80a2581b6/"><BsLinkedin /></a>
+            </li>
+            <li>
+                <a href="https://github.com/chawthorne02"><BsGithub /></a>
+            </li>
+        </ul>
+    )
+}
