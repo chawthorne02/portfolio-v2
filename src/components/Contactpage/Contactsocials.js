cc64a451ed6f1f36eee8fbc2eceab7810ff01268
Corey Hawthorne <chawthorne02@gmail.com>
2023-04-16 13:52:44 -0400

    styled social links

--- src/components/Contactpage/Contactsocials.js
+++ src/components/Contactpage/Contactsocials.js
@@ -10,10 +10,14 @@ function Contactsocials() {
     return (
         <ul className="contact-socials">
             <li>
-                <a href="https://www.linkedin.com/in/corey-hawthorne-80a2581b6/"><BsLinkedin /></a>
+                <a href="https://www.linkedin.com/in/corey-hawthorne-80a2581b6/" className="linkedin">
+                    <BsLinkedin size={70}/>
+                    </a>
             </li>
             <li>
-                <a href="https://github.com/chawthorne02"><BsGithub /></a>
+                <a href="https://github.com/chawthorne02" className="github">
+                    <BsGithub size={70}/>
+                    </a>
             </li>
         </ul>
     )
