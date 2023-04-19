316120f04cf99961b10d2aeb9c416eb18bbb53da
Corey Hawthorne <chawthorne02@gmail.com>
2023-04-19 14:15:07 -0400

    added projects to carousel

--- src/components/Projectspage/Projectspage.js
+++ src/components/Projectspage/Projectspage.js
@@ -1,5 +1,8 @@
 import Carousel from 'react-bootstrap/Carousel';
-
+import Project1 from "../../images/Project1.jpeg"
+import Project2 from "../../images/Project2.jpeg"
+import Project3 from "../../images/Project3.jpeg"
+import Project4 from "../../images/Project4.jpeg"
 
 
 function Projectspage() {
@@ -8,40 +11,31 @@ function Projectspage() {
         <Carousel>
         <Carousel.Item>
           <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=First slide&bg=373940"
+            className="project"
+            src={Project1} height={600}
             alt="First slide"
           />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Second slide&bg=282c34"
+            className="project"
+            src={Project2} height={600}
             alt="Second slide"
           />
-  
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Third slide&bg=20232a"
+            className="project"
+            src={Project3} height={600}
             alt="Third slide"
           />
-  
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="project"
+            src={Project4} height={600}
+            alt="Fourth slide"
+          />
         </Carousel.Item>
       </Carousel>
     )
