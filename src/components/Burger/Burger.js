9705de73a93c70b8d7e2f78a1fdfe30bb16ca4f1
Corey Hawthorne <chawthorne02@gmail.com>
2023-05-02 13:41:25 -0400

    created stylesheet for hamburger menu

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,18 @@
+function Burger() {
+
+
+
+
+
+
+    return (
+        <>
+            <section className="burger">
+                <span className="bar"></span>
+                <span className="bar"></span>
+                <span className="bar"></span>
+                <span className="bar"></span>
+            </section>
+        </>
+    )
+}
